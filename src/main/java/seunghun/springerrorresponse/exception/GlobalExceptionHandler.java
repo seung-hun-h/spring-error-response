cf55713e3bd0bf3ecc15948dcc1168a2c71a83b4
seung-hun-h <dau4723@gmail.com>
2025-12-09 18:44:13 +0900

    검증 에러 응답

--- src/main/java/seunghun/springerrorresponse/exception/GlobalExceptionHandler.java
+++ src/main/java/seunghun/springerrorresponse/exception/GlobalExceptionHandler.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
+// @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(UnsupportedOperationException.class)
